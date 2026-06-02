d5e0a295419af4d5488446b01747d7dadbd702d8
Levil9490 <levil9490@gmail.com>
2026-06-02 20:29:19 -0300

    first commit

---
+++ src/main/java/br/com/levilima/loja/GerenciamentoProdutos.java
@@ -0,0 +1,113 @@
+package br.com.levilima.loja;
+
+/**
+*
+* @author levi
+*/
+public class GerenciamentoProdutos {
+    private Produto produtos[];
+    private int tamanho;
+    private int id;
+    static int CODIGO = 200;
+    private String carrinho;
+    private double total;
+
+    public GerenciamentoProdutos(int tamanho) {
+        this.tamanho = tamanho;
+        this.produtos = new Produto[this.tamanho];
+        this.id = 0;
+        this.carrinho = "";
+        this.total = 0;
+    }
+
+    public void adicionarProduto(Produto produto) {
+        if(this.id < this.tamanho) {
+            this.produtos[this.id] = produto;
+            ++this.id;
+        }
+    }
+
+    public String listarProdutos() {
+        String conteudo = "";
+
+        for(int i = 0; i < this.id; ++i) conteudo += this.produtos[i].exibirProduto() + "\n\n";
+
+        return conteudo;
+    }
+
+    public Produto buscarProduto(int codigo) {
+        for(int i = 0; i < this.id; ++i) if(this.produtos[i].getCodigo() == codigo) return this.produtos[i];
+
+        return null;
+    }
+
+    public void excluirProduto(int codigo) {
+        for(int i = 0; i < this.id; ++i) {
+            if(this.produtos[i].getCodigo() == codigo) {
+                if(i < (this.id - 1)) {
+                    for(int j = i; j < (this.id - 1); ++j) this.produtos[j] = this.produtos[j + 1];
+                    --this.id;
+                    return;
+                }
+            }
+        }
+    }
+
+    public void atualizarProduto(int codigo, String nome, double preco, int quantidade, String vencimento, String categoria, String descricao) {
+        for(int i = 0; i < this.id; ++i) {
+            if(codigo == this.produtos[i].getCodigo()) {
+                this.produtos[i].setNome(nome);
+                this.produtos[i].setPreco(preco);
+                this.produtos[i].setQuantidade(quantidade);
+                this.produtos[i].setVencimento(vencimento);
+                this.produtos[i].setCategoria(categoria);
+                this.produtos[i].setDescricao(descricao);
+            }
+        }
+    }
+
+    public void adicionarCarrinho(Produto produto, int quantidade) {
+        this.carrinho += "\nCodigo: " + produto.getCodigo() + "\tNome: " + produto.getNome() + "\tQuantidade: " + quantidade + "\tPreço: " + (produto.getPreco() * quantidade) + "\n";
+        this.total += (produto.getPreco() * quantidade);
+        produto.setQuantidade(produto.getQuantidade() - quantidade);
+    }
+
+    public String verCarrinho() {
+        return this.carrinho + "Total: " + this.total + "\n";
+    }
+
+    public String verificarEstoque() {
+        String conteudo = "";
+
+        if(this.id == 0) return "Nenhum produto no estoque";
+
+        for(int i = 0; i < this.id; ++i)
+                if(this.produtos[i].getQuantidade() == 0)
+                        conteudo += "Código: " + this.produtos[i].getCodigo() + " Nome: " + this.produtos[i].getNome() + " Descrição: " + this.produtos[i].getDescricao() + " Quantidade: " + this.produtos[i].getQuantidade() + " Preço: " + this.produtos[i].getPreco() + "\n";
+
+        if(conteudo.equals("")) return "Os produtos possuem itens";
+
+        return conteudo;
+    }
+
+    public String verificarEstoque(int margem) {
+        String conteudo = "";
+
+        if(this.id == 0) return "Nenhum produto no estoque";
+
+        for(int i = 0; i < this.id; ++i)
+            if(this.produtos[i].getQuantidade() <= margem)
+                    conteudo += "Código: " + this.produtos[i].getCodigo() + " Nome: " + this.produtos[i].getNome() + " Descrição: " + this.produtos[i].getDescricao() + " Quantidade: " + this.produtos[i].getQuantidade() + " Preço: " + this.produtos[i].getPreco() + "\n";
+
+        if(conteudo.equals("")) return "Os produtos possuem itens";
+
+        return conteudo;
+    }
+
+    public String finalizarCompra() {
+        String conteudo = this.carrinho + "\nTotal" + this.total + "\nCompra concluída com sucesso!\n";
+        this.carrinho = "";
+        this.total = 0;
+        return conteudo;
+    }
+}
---
+++ src/main/java/br/com/levilima/loja/Loja.java
@@ -0,0 +1,119 @@
+package br.com.levilima.loja;
+
+/**
+ *
+ * @author levi
+ */
+import javax.swing.JOptionPane;
+
+public class Loja {
+    public static void main(String[] args) {
+        int quantidade;
+        String nome, descricao, vencimento, categoria;
+        double preco;
+        boolean controle = true;
+        int busca;
+        Produto produto;
+
+        GerenciamentoProdutos genProd = new GerenciamentoProdutos(5);
+
+        while(controle) {
+            int opcao = Integer.parseInt(JOptionPane.showInputDialog(null, "Sistema de gerenciamento de produtos\n" +
+                "Escolha uma opcao:\n" +
+                "1 - Inserir produto\n" +
+                "2 - Listar produtos\n" +
+                "3 - Buscar produto\n" +
+                "4 - Excluir produto\n" +
+                "5 - Editar produto\n" +
+                "6 - Adicionar ao carrinho\n" +
+                "7 - Ver carrinho\n" +
+                "8 - Verificar estoque\n" +
+                "9 - Finalizar compra\n" +
+                "0 - Sair"));
+
+            switch(opcao) {
+                case 0:
+                    controle = false;
+                    JOptionPane.showMessageDialog(null, "Saindo do programa...");
+                    break;
+                case 1:
+                    nome = JOptionPane.showInputDialog("Digite o nome do produto:");
+                    descricao = JOptionPane.showInputDialog("Digite a descricao do produto:");
+                    preco = Double.parseDouble(JOptionPane.showInputDialog("Digite o preco do produto:"));
+                    vencimento = JOptionPane.showInputDialog("Digite o vencimento do produto:");
+                    quantidade = Integer.parseInt(JOptionPane.showInputDialog("Digite a quantidade do produto:"));
+                    categoria = JOptionPane.showInputDialog("Digite a categoria do produto:");
+
+                    produto = new Produto(nome, descricao, preco, vencimento, quantidade, categoria);
+                    genProd.adicionarProduto(produto);
+
+                    break;
+                case 2:
+                    JOptionPane.showMessageDialog(null, genProd.listarProdutos());
+                    break;
+                case 3:
+                    busca = Integer.parseInt(JOptionPane.showInputDialog("Digite o código do produto:"));
+                    produto = genProd.buscarProduto(busca);
+
+                    if(genProd.buscarProduto(busca) == null) {
+                        JOptionPane.showMessageDialog(null, "Produto não encontrado");
+                    } else {
+                        JOptionPane.showMessageDialog(null, produto.exibirProduto());
+                    }
+
+                    break;
+                case 4:
+                    busca = Integer.parseInt(JOptionPane.showInputDialog("Digite o código do produto:"));
+                    genProd.excluirProduto(busca);
+                    break;
+                case 5:
+                    busca = Integer.parseInt(JOptionPane.showInputDialog("Digite o código do produto: "));
+                    produto = genProd.buscarProduto(busca);
+
+                    if(produto == null) {
+                        JOptionPane.showMessageDialog(null, "Produto não encontrado!");
+                    } else {
+                        nome = JOptionPane.showInputDialog("Digite o nome do produto:");
+                        descricao = JOptionPane.showInputDialog("Digite a descricao do produto:");
+                        preco = Double.parseDouble(JOptionPane.showInputDialog("Digite o preco do produto:"));
+                        vencimento = JOptionPane.showInputDialog("Digite o vencimento do produto:");
+                        quantidade = Integer.parseInt(JOptionPane.showInputDialog("Digite a quantidade do produto:"));
+                        categoria = JOptionPane.showInputDialog("Digite a categoria do produto:");
+                        genProd.atualizarProduto(busca, nome, preco, quantidade, vencimento, categoria, descricao);
+                    }
+                    break;
+                case 6:
+                    busca = Integer.parseInt(JOptionPane.showInputDialog("Digite o código do produto: "));
+                    produto = genProd.buscarProduto(busca);
+
+                    if(produto == null) {
+                        JOptionPane.showMessageDialog(null, "Produto não encontrado!");
+                    } else {
+                        quantidade = Integer.parseInt(JOptionPane.showInputDialog("Digite a quantidade do produto:"));
+                        if(quantidade > produto.getQuantidade()) {
+                            JOptionPane.showMessageDialog(null, "Quantidade indisponível.");
+                        } else {
+                            genProd.adicionarCarrinho(produto, quantidade);
+                        }
+                    }
+
+                    break;
+                case 7:
+                    JOptionPane.showMessageDialog(null, genProd.verCarrinho());
+                    break;
+                case 8:
+                    int margem = Integer.parseInt(JOptionPane.showInputDialog("Digite a margem desejada:"));
+
+                    if(margem == 0) {
+                        JOptionPane.showMessageDialog(null, genProd.verificarEstoque());
+                    } else {
+                        JOptionPane.showMessageDialog(null, genProd.verificarEstoque(margem));
+                    }
+
+                    break;
+                case 9:
+                    JOptionPane.showMessageDialog(null, genProd.finalizarCompra());
+            }
+        }
+    }
+}
